613023b4383ebef5351d8b180dae3325f9edcae2
지나가던똥개 <chlalswp0222@gmail.com>
2021-02-09 00:32:14 +0000

    object_data complete. next is moving between dataNdata

--- Screens/TextEditor.js
+++ Screens/TextEditor.js
@@ -18,9 +18,8 @@ export default class TextEditor extends React.Component {
         TextFromWeb : '', //
         isLoading: true,
         //Url: TextStore.UrlForFetch,
-		Url : 'namu.wiki/w/%ED%8A%B8%EB%A3%A8%EB%A8%BC%20%EB%8F%85%ED%8A%B8%EB%A6%B0', //실험에 쓸만한 임시주소. 나중에 위로 바꿔라.
+		Url : 'https://namu.wiki/w/%ED%8A%B8%EB%A3%A8%EB%A8%BC%20%EB%8F%85%ED%8A%B8%EB%A6%B0', //실험에 쓸만한 임시주소. 나중에 위로 바꿔라.
         InputText : '',
-		
 		
 		TextToSpeech : '', //읽을 최종 결과물
 		selectedVoice : '', 
@@ -28,7 +27,7 @@ export default class TextEditor extends React.Component {
 		speechRate : TextStore.speechRate,
 		speechPitch : TextStore.speechPitch,
 		
-		content_index : 1, // 제목 제외한 본문 요소들의 순번용
+		content_index : 0, // 제목 포함한 본문 요소들의 순번용
 		footnote_index  : 1 // 여러 챕터 전체에 걸친 주석과 연결될 번호 
     };
 
@@ -64,11 +63,14 @@ export default class TextEditor extends React.Component {
 
         $(".footnote-list").each(function (index, element) {
 			var index_plus = index + 1
-			var index_n_text = $(element).text() // [1] 이 당시에는 셀리카도 FR이었다.
 			var index_string = '[' + String(index+1) + ']' // [1]
+			
+			var index_n_text = $(element).text() // [1] 이 당시에는 셀리카도 FR이었다.
 			var footnote_text = index_n_text.split(index_string)[1] // "이 당시에는 셀리카도 FR이었다."
-        	var footnote_obj = { index_plus_1 : { footnote_string : index_string , text : footnote_text} }
-			var footnote_data = { ...footnote_data, footnote_obj}
+        	
+			//var footnote_obj = { [index_plus] : { footnote_string : index_string , text : footnote_text} }
+			footnote_data[index_plus] = { footnote_string : index_string , text : footnote_text}
+			//var footnote_data = { ...footnote_data, footnote_obj}
 			// return footnote_data 이때 리턴을 써야했던가...???
 		})
 		
@@ -79,14 +81,16 @@ export default class TextEditor extends React.Component {
 		
 		// 아래의 각 요소는 ["제목", "내용텍스트"]
 		//wiki.each(function(index, element) {
-		for (var index_of_wiki=0; index_of_wiki<1; index_of_wiki++){
-			console.log(index_of_wiki + "이 현재의 인덱스오브위키")
+		for (var index_of_wiki=0; index_of_wiki<2; index_of_wiki++){
+			console.log(index_of_wiki + "이 현재의 인덱스오브위키[=챕터]")
 			var element_wiki = wiki[index_of_wiki]
 			
 			// 인덱스는 챕터를 나타내게 된다.
 			// 제목은 모든 챕터에서 처음으로 올테니 0으로 인덱스를 지정받으며 값으로 타입:헤딩과 구성요소의 전자를 텍스트로 가진다.
-			var heading_data = { index : { 0 : {type : "heading", text : element_wiki[0] }}}
-			var wiki_data = {...wiki_data, heading_data}
+			
+			var content_index = this.state.content_index // 챕터 내에서 제목[=0] 다음으로의 순번. 최초값 1
+			wiki_data[index_of_wiki] = {[content_index] : {type : "heading", text : element_wiki[0] }}
+			this.setState({ content_index : content_index + 1})
 			
 			// 의도대로라면 위키데이터 = { 0: { 0 : { 타입:제목, 텍스트:내용}, ??? }} 이 된다. 이제 본문을 다루며 뒤의 ???를 채워나간다.
 			// 제목의 데이터를 넣었으니 
@@ -103,8 +107,8 @@ export default class TextEditor extends React.Component {
 			
 			// 한 챕터의 내용의 문장들을 . 을 기준으로 나눈 어레이.
 			var array_of_contents = element_wiki[1].split(".")
-			console.log(". 을 기준으로 나뉘어질 전체 문장의 합은 ... " + array_of_contents)
-			// 여기까진 오케이....
+			array_of_contents.pop() // . 을 기준으로 챕터를 나누면 마지막 문장의 ~~. 도 취급되서 마지막에 빈 텍스트가 오브젝트로 들어가게됨. 이를 삭제
+
 			
 			// 위의 . 기준으로 나뉜 문단의 문장들은 몇 챕터인지를 공유해야하니, 현재 챕터를 나타내는 인덱스를 변수로 삼언하고 아래의 each의 인수로 건내주자
 			var chapter_index = index_of_wiki
@@ -115,42 +119,36 @@ export default class TextEditor extends React.Component {
 				var element_array = array_of_contents[index_array_of_contents]
 				
 				// 위키_데이터를 위의 평션의 인수로 챕터를 추가해야하는가? 아니면 없어도 알아서 하위함수는 상위함수에서 선언한 변수값을 반영하는가???
-				var content_index = index_array_of_contents + this.state.content_index // 챕터 내에서 제목[=0] 다음으로의 순번. 최초값 1
+				//var content_index = this.state.content_index // 챕터 내에서 제목[=0] 다음으로의 순번. 최초값 1
+				content_index = this.state.content_index // 챕터 내에서 제목[=0] 다음으로의 순번. 최초값 1
 				var footnote_index = this.state.footnote_index // 전 챕터에 걸쳐 있을 주석에 대한 번호. [1]부터 시작해야하니 최초값 1
-				var footnote_string = "["+footnote_index.string+"]" // 최초의 경우에는 "[1]"
+				var footnote_string = "["+String(footnote_index)+"]" // 최초의 경우에는 "[1]"
 				var after_footnote = "" // 한 문장에 여러 주석이 있는 경우 뒤에 남는 문장도 다시 여러번 자르는 과정 필요. 이때 돌려가며 사용될 변수
 				
 				//만약 문장에 [1]이 없다면 주석없는 문장이니 통채로 오브젝트화하고 머지
-				if (!element_array.includes("["+footnote_index.string+"]")){
-					var sentences = { chapter_index : { content_index : {type : "sentence", text : element_array}} }
-					console.log("주석없는 것의 전체 텍스트 " + sentences.chapter_index.content_index.text)
-					wiki_data = { ...wiki_data, ...sentences} 
-					//생각대로라면 위키데이터 = { 0: { 0 : { 타입:제목, 텍스트:내용}, 1 : {타입:문장, 텍스트:내용}}} 로 0번째 챕터가 마무리 되는 셈이다...
-					
+				if (!element_array.includes(footnote_string)){
+					wiki_data[chapter_index][content_index] = {type : "sentence", text : element_array}
 					this.setState({ content_index : content_index + 1})
-					console.log(wiki_data) //확인용으로 출력한번 시켜보자.
 					continue
-					// 여기까진 오케이...
 				} else {
-					console.log("문장에 주석이 있으니 아래로 진행")
 				}
 				//위의 브레이크가 발동 안함 = 주석이 있는 문장의 경우 반복문 
 				while (element_array.includes(footnote_string)){
-					var split_sentnece = element_array.split(footnote_string) // 주석을 기준으로 문장을 나눈다. "그는 / [1] / 언제나[2] 그래왔다."
-					var before_footnote = { chapter_index: { content_index :{ type : "sentence", text : split_sentnece[0]}} }
-					console.log("주석있는 것의 앞부분은 " + before_footnote.chapter_index.text) // "그는"
-					wiki_data = { ...wiki_data, ...before_footnote}
 					
-					//////////////////////////////////////
+					var split_sentnece = element_array.split(footnote_string) // 주석을 기준으로 문장을 나눈다. "그는 / [1] / 언제나[2] 그래왔다."
+					
+					wiki_data[chapter_index][content_index] = { type : "sentence", text : split_sentnece[0]}
+					
 					//footnote_data = { 1 : { footnote_string: [1], text : "텍스트" }
 					// 앞에서 한거 반복인데...나중에 삭제해도 돌아가는지 확인하자
 					var footnote_index  = this.state.footnote_index // 스테이트의 인덱스와 동기화. 최초값은 1
-					var footnote_text = footnote_data.footnote_index.text
-					var footnote_string = footnote_data.footnote_index.footnote_string
-					console.log("주석. 내용은 " + footnote_text + ". 번호는 " + footnote_string)
+					var footnote_text = footnote_data[footnote_index].text
+					var footnote_string = footnote_data[footnote_index].footnote_string
 					
-					var footnote_obj = { chapter_index : { content_index : { type:"footnote", text : footnote_text}}}
-					wiki_data = {...wiki_data, ...footnote_obj}
+					//var footnote_obj = { chapter_index : { content_index : { type:"footnote", text : footnote_text}}}
+					//wiki_data = {...wiki_data, ...footnote_obj}
+					wiki_data[chapter_index][content_index] = { type:"footnote", text : footnote_text}
+					
 					footnote_index += 1 // 이걸로 풋노트스트링도 변하면서 자동으로 [2]에 맞춘 루프구문이 시행될까...? 
 					content_index += 1 // 주석도 컨텐츠의 번호 대상이니 추가해준다.
 					////위의 내용과 주석가져오기를 겷합해라////
@@ -163,20 +161,19 @@ export default class TextEditor extends React.Component {
 					//추가된 번호를 스테이트에 업뎃. 주석이 포함된 한문장이 완료되서 다음 문장을 다룰때마다 초기에 이 스테이트값을 가져와서 대입함으로서 증가된 번호를 매 문장마다 반영.
 					this.setState({footnote_index : footnote_index, content_index : content_index}) 
 				}
-				// 브레이크도 안거치고 위 조건문도 통과했으면 남는 건 주석있는 문장의 마지막 텍스트다. "그래왔다"
+				// 컨티뉴 통과했으니 주석이 있는 문장인데 위의 주석 체크조건문도 통과했으면 남는 건 주석있는 문장의 마지막 텍스트다.
 				if (after_footnote !== '' || '.') { // 문장 마지막 주석 뒤에 마침표가 아닌 텍스트 내용이 있다면...
 					var content_index = this.state.content_index // 챕터 내에서 몇번째인지를 반영하기 위해서 동기화시켜주고...
 					
-					var lastPartOfSentence = { chapter_index : { content_index : { type : "sentence", text : after_footnote}}}
-					wiki_data = { ...wiki_data, ...lastPartOfSentence}
+					//var lastPartOfSentence = { chapter_index : { content_index : { type : "sentence", text : after_footnote}}}
+					//wiki_data = { ...wiki_data, ...lastPartOfSentence}
+					wiki_data[chapter_index][content_index] = { type : "sentence", text : after_footnote}
 					this.setState({content_index : content_index + 1})
-					console.log("주석뒤에 남은부분은 " + lastPartOfSentence.chapter_index.text)
 				} 
-				// 이제 한문장을 다 마무리 했으니 다음 문장을 대상으로 해야함. each가 알아서 돌아감. 따로 건들건 없나???
-				console.log("한문장 완료.")
-				console.log(wiki_data) //확인용으로 출력한번 시켜보자.
-				//return false // 확인용으로 반복문 탈출. 한챕터만 다루고 마치도록...
 			}
+			this.setState({content_index : 0})
+			console.log(" 다음 챕터로 넘어가기 전 마지막으로 위키데이터 오브젝트 전체 출력")
+			console.log(wiki_data) //확인용으로 출력한번 시켜보자.
 		}
         let txtList = []
         for (let id in wiki){
